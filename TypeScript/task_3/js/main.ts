cb09f93a4ce8e104fd2bf2f7759564df0033294f
Thomas Maye <thomas.maye.33@gmail.com>
2025-01-27 16:14:42 +0100

    Task 8 : Add CRUD operations and interfaces for task 3 implementation

---
+++ TypeScript/task_3/js/main.ts
@@ -0,0 +1,26 @@
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {firstName: "Guillaume", lastName: "Salva", age: 23};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
+
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj)
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
+
+CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
+
+CRUD.deleteRow(125);
+// Delete row id 125
